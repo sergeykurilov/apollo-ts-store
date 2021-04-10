0afeafa9acb30598ff36f837119497a807481361
Sergey <kurilovsergey15@gmail.com>
2021-04-10 03:47:15 +0300

    server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import { connectDatabase } from "./database";
 const app = express();
 
 //
-
+///asdadasd
 const mount = async (app: Application) => {
   const db = await connectDatabase();
   const server = new ApolloServer({
